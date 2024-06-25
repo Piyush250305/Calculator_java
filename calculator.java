1869474242b2da3a6a93140816f32f357b8b19e0
Piyush Churhe <140887498+Piyush250305@users.noreply.github.com>
2024-06-26 00:43:46 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,34 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
 package internship;
 import java.util.Scanner;
 
-/**
- *
- * @author asus
- */
 public class calculator {
     public void calculate()
     {
         Scanner scanner = new Scanner(System.in);
         
-        // Taking two numbers as input
         System.out.print("Enter the first number: ");
         double num1 = scanner.nextDouble();
         
         System.out.print("Enter the second number: ");
         double num2 = scanner.nextDouble();
         
-        // Taking the operation as input
         System.out.print("Enter the operation (+, -, *, /, %): ");
         char operation = scanner.next().charAt(0);
         
         double result = 0;
         
-        // Performing the chosen operation
         switch (operation) {
             case '+':
                 result = num1 + num2;
@@ -58,7 +46,6 @@ public class calculator {
         scanner.close();
 }
 public static void main(String[] args) {
-        // TODO code application logic here
         calculator c1 = new calculator(); 
         c1.calculate();
 }
